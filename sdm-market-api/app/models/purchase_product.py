04e6ffcb1adac320200d2d28a908a593bc4436c9
victorandradeap <victorandradeap@gmail.com>
2025-05-13 14:29:27 -0300

    fix: fix db relationship

--- sdm-market-api/app/models/purchase_product.py
+++ sdm-market-api/app/models/purchase_product.py
@@ -5,12 +5,14 @@ purchase_product = db.Table('purchase_product',
     db.Column('purchase_id', db.Integer, db.ForeignKey('purchases.id'), primary_key=True),
     db.Column('product_id', db.Integer, db.ForeignKey('products.id'), primary_key=True),
     db.Column('quantity', db.Integer, nullable=False),
-    db.Column('unit_price', db.Float, nullable=False)  # Preço no momento da compra
+    db.Column('unit_price', db.Float, nullable=False),  # Preço no momento da compra
+    extend_existing=True
 )
 
 # Modelo ORM para a mesma tabela, permitindo acesso aos campos
 class PurchaseProduct(db.Model):
     __tablename__ = 'purchase_product'
+    __table_args__ = {'extend_existing': True}
     
     purchase_id = db.Column(db.Integer, db.ForeignKey('purchases.id'), primary_key=True)
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
